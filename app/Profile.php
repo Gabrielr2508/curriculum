d00fc49607bc974657a61200813d1c78fe945b9d
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2019-06-10 23:02:34 -0300

    Adjust mutators of profile model

--- app/Profile.php
+++ app/Profile.php
@@ -20,14 +20,24 @@ class Profile extends \Moloquent {
 	];
 
 
-	public function setEmailAtributte($value)
+	public function setEmailAttribute($value)
 	{
 		$this->attributes['email'] = strtolower($value);
 	}
 
-	public function setPhoneAtributte($value)
+	public function setPhoneAttribute($value)
 	{
 		$this->attributes['phone'] = preg_replace("/\D+/", "", $value);
+	}
+
+	public function setIsInternshipAttribute($value)
+	{
+		$this->attributes['is_internship'] = filter_var($value, FILTER_VALIDATE_BOOLEAN);
+	}
+
+	public function setNameAttribute($value)
+	{
+		$this->attributes['name'] = ucwords($value);
 	}
 
 	public function curriculum()
@@ -35,8 +45,4 @@ class Profile extends \Moloquent {
 		return $this->hasMany('App\Curriculum');
 	}
 
-	public function setNameAttribute($value)
-    {
-        $this->attributes['name'] = ucwords($value);
-    }
 }
